334b7721460400125300b3319a2d6c6510e2aaae
Scott-Dupasquier <sdupasqu@ualberta.ca>
2020-05-18 19:13:52 -0600

    General styling, added a main menu. Closes #17

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public static SceneChanger sceneChanger;
+
+    // Singleton object, on awake make sure we don't repeat
+    void Awake()
+    {
+        if (!sceneChanger)
+        {
+            sceneChanger = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Changes to main menu
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Changes to game screen
+    public void GameScreen()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // Exit the application
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
